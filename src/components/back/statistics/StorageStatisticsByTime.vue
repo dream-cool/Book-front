df5cd1183e1e43e8a4f3393976b6e8c6ab7ddfcc
dream-cool <1142170725@qq.com>
2020-03-05 21:25:17 +0800

    完善统计管理

--- src/components/back/statistics/StorageStatisticsByTime.vue
+++ src/components/back/statistics/StorageStatisticsByTime.vue
@@ -7,11 +7,26 @@
       :settings="chartSettings">
       </ve-line>
 
+      
+
+      <el-date-picker style="margin-left: 30%"
+        v-model="timeRange"
+        type="daterange"
+        align="right"
+        value-format="timestamp"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        @change="getData"
+        :picker-options="pickerOptions">
+      </el-date-picker>
+
       <el-radio-group @change="handleChangeTime" v-model="timeslot" >
                 <el-radio :label="'YEAR'">年</el-radio>
                 <el-radio :label="'MONTH'">月</el-radio>
                 <el-radio :label="'DAY'">日</el-radio>
       </el-radio-group>
+      
   </div>
 </template>
 
@@ -35,13 +50,45 @@ export default {
       },
       loading: false,
       dataEmpty: false,
-      timeslot: 'DAY'
+      timeslot: 'DAY',
+      pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        },
+        timeRange: []
     }
   },
   methods: {
     getData () {
-      axios.get('/statistics/bookStorageByTime?timeSlot=' + this.timeslot).then(res => {
+      axios.get(
+        '/statistics/bookStorageByTime?timeSlot=' 
+        + this.timeslot + '&start='+this.timeRange[0]+'&end='+this.timeRange[1]
+      ).then(res => {
         this.chartData.rows = res.data.data
+        console.log(this.timeRange)
       })
     },
 
@@ -51,6 +98,7 @@ export default {
 
   },
   created () {
+    
     this.getData()
   }
 }
