df632edc5a98db9b67858914e8c1ef2a1e455c01
dream-cool <1142170725@qq.com>
2020-04-20 23:04:11 +0800

    完成用户收藏分组的修改和删除

--- src/components/front/UserCollection.vue
+++ src/components/front/UserCollection.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="hello" >
-      <el-tabs type="border-card"  v-model="active"
+      <el-tabs    v-model="active"
         @tab-click="handleTabChange">
+        
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-edit"></i> 编辑收藏</span>
+            <el-card title="我的收藏" 
+              style="margin-left: 20%;width: 50%;height: 800px">
+              <el-tag v-for="(item,index) in collectGroupList" :key="index"
+                      style="width: 250px;margin-left: 100px;margin-top: 20px"
+                closable
+                @close="handleDelete(item)"
+                @click="handleEdit(item)"
+                type="info"
+                >
+                {{item.name}}
+              </el-tag>
+              <br>
+              <el-input
+                  placeholder="新建收藏分组"
+                  v-model="collectGroup.name"
+                  style="margin-top: 50px;margin-left:100px;width:250px"
+                  clearable>
+                </el-input>
+              <el-button icon="el-icon-plus" style="margin-left:10px" @click="addCollectGroup"></el-button>
+            </el-card>
+        </el-tab-pane>
+
         <el-tab-pane v-for="(item,index) in collectGroupList" :key="index"
             :label="item.name" :name="item.name">
+          <span slot="label"><i class="el-icon-folder"></i> {{item.name}}</span>
+
           <el-timeline >
                 <el-timeline-item placement="top"
                     v-for="(item,index) in useCollectionListGroupTime"
@@ -32,6 +59,7 @@
                 <p v-if="messageList.length == total">我也是有底线的</p> -->
             </el-timeline>
         </el-tab-pane>
+       
       </el-tabs>
   </div>
 </template>
@@ -57,7 +85,11 @@ export default {
         userId: null,
         groupName: '默认收藏夹',
         isCollect: true
-      }
+      },
+      collectGroup: {
+        name: null
+      },
+
     }
   },
   created () {
@@ -65,13 +97,46 @@ export default {
     this.useCollection.userId = this.user.userId
     this.getCollectGroupInfo()
     this.getUseCollectionInfoGroupTime(this.useCollection)
+
   },
   methods: {
-    aa(){
-      console.log(11)
+    handleEdit(item){
+      this.$prompt('请输入分组新名称', '修改分组', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputValue: item.name,
+          inputPattern: /\S/,
+          inputErrorMessage: '分组名不能为空'
+        }).then(({ value }) => {
+          item.name = value
+          this.updateCollectionGroup(item)
+        }).catch(() => {
+              
+        });
+    },
+    handleDelete(item){
+      this.$confirm('删除分组，将删除该分组下的所有收藏信息，确认删除？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          axios.delete('/collectionGroup/'+item.collectionGroupId).then( res => {
+            if(res && res.data.code == 200){
+              this.getCollectGroupInfo()
+              this.$message(res.data.message)
+            } else {
+              this.$message.error(res.data.message)
+            }
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
     },
     handleTabsEdit () {
-
+      
     },
     handleTabChange () {
       this.useCollection.groupName = this.active
@@ -116,6 +181,34 @@ export default {
     goToBookDetail (bookId) {
       document.body.style.overflow = null
       this.$router.push({path: '/front/bookDetail/' + bookId})
+    },
+    updateCollectionGroup (collectionGroup) {
+      if (collectionGroup != null) {
+        axios.put('/collectionGroup', collectionGroup)
+          .then(res => {
+            if (res && res.data.code === 200) {
+              this.getCollectGroupInfo()
+              this.$message(res.data.message)
+            } else {
+              this.$message.error(res.data.message)
+            }
+            this.getCollectGroupInfo()
+          })
+      }
+    },
+    addCollectGroup () {
+      axios.post('/collectionGroup', {
+        name: this.collectGroup.name,
+        userId: this.user.userId
+      }).then(res => {
+        if (res.data.code === 200) {
+          this.collectGroup.name = null
+          this.getCollectGroupInfo()
+          this.$message(res.data.message)
+        } else {
+          this.$message.error(res.data.message)
+        }
+      })
     }
 
   }
--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ import 'nprogress/nprogress.css'
 NProgress.inc(0.5)
 NProgress.configure({ easing: 'ease', speed: 800, showSpinner: false })
 
+// axios.defaults.baseURL = 'http://39.97.239.108:8090'
 axios.defaults.baseURL = 'http://localhost:8090'
 axios.defaults.timeout = 5000
 
